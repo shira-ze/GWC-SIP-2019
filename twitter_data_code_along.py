ec22e805988817a818886bd1a431b33eb24f6982
shira <shirazelefsky@gmail.com>
2019-07-19 11:00:53 -0400

    added a few hist plots for word and letter counts

--- twitter_data_code_along.py
+++ twitter_data_code_along.py
@@ -11,34 +11,45 @@ import json
 from textblob import TextBlob
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+'''
+def numOfLetter(tw, letter):
+	count = 0
+	for l in tw:
+		if l == letter or l.lower() == letter:
+			#print(l)
+			count += 1
+	return count
+'''
+#
 
-# def numOfLetter(tw, letter):
-# 	count = 0
-# 	for l in tw:
-# 		if l == letter or l.lower() == letter:
-# 			#print(l)
-# 			count += 1
-# 	return count
-#YES
-#
-#
-# # Next we want to open the JSON file. We tag this file as
-# # "r" read only because we are only going to look at the data.
+# Next we want to open the JSON file. We tag this file as
+# "r" read only because we are only going to look at the data.
 tweetFile = open("tweets_small.json", "r")
 #
+def countLetter(string, letter):
+	counter = 0
+	for let in string:
+		if let.lower() == letter:
+			counter += 1
+	return counter
+def wordCount(stringOfTweet, string1):
+	counter = 0
+	string1 = string1.lower()
+	wordList = stringOfTweet.split(" ")#the .split turns a string into list of all words by making every space sep into a new index
+	for item in wordList:
+		if item == string1:
+			counter += 1
+	return counter
+
 # # We use the JSON library to get data from the file as JSON data. (load function only exists bc json library was imported)
 tweetData = json.load(tweetFile)
 tweetFile.close()
-# print(type(tweetData))
-# print(type(tweetData[0]))
-# # list(newdict.keys())
 # print(list(tweetData[0].keys()))
 # #this (above) shows the te name of all the keys in the list's index 0, such as ['created_at', 'favorite_count', 'hashtags', 'id', 'id_str', 'lang', 'retweet_count', 'source', 'text', 'truncated', 'urls', 'user', 'user_mentions']
 # #to see the value of one of those keys:
 # # print(tweetData[0]["created_at"])
-#
 '''
-# #HERE (BELOW) IS THE CODE TO FIND THE AVERAGE
+#HERE (BELOW) IS THE CODE TO FIND THE AVERAGE
 # sum = 0
 # num = 0 #the amount of tweets total that have favorites- the total when divide sum by total
 # for i in range(len(tweetData)):
@@ -57,39 +68,21 @@ tweetFile.close()
 # print("here is avg!!!!!!!!!!")
 # print(average)
 '''
-#
+
+
+#creates list of only the text in tweets called tweet list
 tweetList = []
 for i in range(len(tweetData)):
 	tweetList.append(tweetData [i]["text"])
 # print(tweetList)
-#
-# tweetID = []
-# for i in range(len(tweetData)):
-# 	tweetList.append(tweetData [i]["id"])
-# print(type(tweetID))
-#
-#
-#
 
-polarityList = []
-for item in tweetList:
-	blob1 = TextBlob(item)
-	polar1 = blob1.polarity
-	polarityList.append(polar1)
-print(polarityList)
-#
-# ***THERE SEEMS TO BE SOME ISSUES HERE***
-tweetpol = {}
-dictlist = []
-for i in range(0,len(tweetData)):
-	poldict = {}
-
-	poldict["twitterid"] = tweetData[i]["id"]
-	poldict["polarity"] = polarityList[i]
-	poldict["tweet"] = tweetList[i]
-	dictlist.append(poldict)
-
-# print(dictlist)
+'''
+#I dont really know what the point of this is:
+tweetID = []
+for i in range(len(tweetData)):
+	tweetList.append(tweetData [i]["id"])
+print(type(tweetID))
+'''
 
 #turn all tweets into one long string by taking the tweets in the tweet list and iterat thru it
 tweetstring = " "
@@ -97,6 +90,47 @@ for tweet in tweetList:
 	tweet = tweet + " " #this reassigns tweet to be tweet plus a space
 	tweetstring += tweet
 # print(tweetstring)
+'''
+polarityList = []
+for item in tweetList:
+	blob1 = TextBlob(item)
+	polar1 = blob1.polarity()
+	polarityList.append(polar1)
+# print(polarityList)
+#
+tweetpol = {}
+dictlist = []
+for i in range(0,len(tweetData)):
+	poldict = {}
+	poldict["twitterid"] = tweetData[i]["id"]
+	poldict["polarity"] = polarityList[i]
+	poldict["tweet"] = tweetList[i]
+	dictlist.append(poldict)
+'''
+alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters = sorted(alpha)
+
+occurences = []
+for letter in letters:
+	occurences.append(countLetter(tweetstring, letter))
+'''
+plt.hist(occurences)
+plt.axis([min(occurences), max(occurences), 0, 10])
+plt.show()
+plt.savefig("histogram of letter occurences of a letter in tweetstring")
+'''
+# list of times a word occurs in a tweet:
+wordCountList = []
+for item in tweetList:
+	wordoccurence = wordCount(item, "the")
+	wordCountList.append(wordoccurence)
+n, ins, patches = plt.hist(wordCountList, 50)
+plt.axis([min(wordCountList), max(wordCountList), 0, 50])
+plt.grid(True)
+plt.show()
+plt.savefig("histogram of word occurences of a letter in tweetstring")
+
+# print(wordCountList)
 '''
 # generates wordCloud:
 wordcloud = WordCloud(height = 1000, width = 1000). generate(tweetstring)
@@ -107,25 +141,21 @@ plt.show()
 plt.savefig("shira'schart.png")
 '''
 
-
+'''
 #histogram stuff
-print(polarityList)
+# print(polarityList)
 # print(min(polarityList), max(polarityList))
 plt.hist(polarityList)
 
 plt.axis([-0.55, 1.05, 0, 50]) # first two are min and max for x axis, last two are min max for y axis
 # plt.grid(True)
-plt.show()
+# plt.show()
 plt.savefig("shirachart.png")
 plt.xlabel("polarity")
 plt.ylabel("frequency")
 plt.title('Histogram of polarity of Tweets')
 # plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
-
-
-# new Thing
-# tb = TextBlob("you are compsists") #this returns tb, a "text blob object" which you could do a  lot of things to like print the polarity...
-
+'''
 
 
 #tweet data is the var name for what us now storing all the twiiter data from JSON
